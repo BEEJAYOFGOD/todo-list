010853d3464e6b91618599ecd2e976e0eeef873b
beejayofgod <adexbolaji100@gmail.com>
2025-03-15 03:00:38 +0100

    fixed design issues

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,35 +1,34 @@
 import check from "../images/icon-check.svg";
 import deleteIcon from "../images/icon-cross.svg";
 
-const Todo = ({ todo, toggleTodo, deleteTodo, theme, todos }) => {
+const Todo = ({ todo, toggleTodo, deleteTodo, theme }) => {
   return (
     <>
       {" "}
       <div
-        className={`flex justify-between space-x-4 p-4 ${
+        className={`flex justify-between space-x-4 md:space-x-24 p-4 ${
           theme == "dark" ? "border-b-1" : "border-b-gray-400 border-b-1"
         }`}
       >
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 flex-1">
           <div
-            className={`h-6 w-6 flex justify-center items-center rounded-full ${
+            className={`h-6 aspect-square flex justify-center items-center rounded-full ${
               todo.checked
                 ? "bg-blue-500 bg-gradient-to-br from-blue-500 via-blue-500 to-purple-500"
                 : "border"
             } ${theme == "dark" ? "border-gray-600" : "border-gray-900"}`}
             onClick={() => {
               toggleTodo(todo.id);
-              localStorage.setItem("todos", JSON.stringify(todos));
             }}
           >
             <img
               src={check}
               alt=""
-              className={todo.checked ? "flex" : `hidden`}
+              className={`${todo.checked ? "flex" : "hidden"} `}
             />
           </div>
           <p
-            className={`
+            className={`text-justify w-fit border
             ${todo.checked ? "line-through" : "bold"}
             ${theme === "dark" ? "text-white" : "text-black"}
           `}
